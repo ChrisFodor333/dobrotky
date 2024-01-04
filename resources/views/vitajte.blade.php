033732eea4421aab3572da71e7abbb86eb3f742e
ChrisFodor333 <chrisfodor333@gmail.com>
2024-01-04 18:40:16 +0100

    vitajte

--- resources/views/vitajte.blade.php
+++ resources/views/vitajte.blade.php
@@ -1,10 +1,30 @@
-@include('dopyt/header')
 
-<style>
-.text-red {
-  color:red !important;
-}
-</style>
+<head>
+  <title>Dobroty od dobrotky</title>
+  <meta charset="utf-8" />
+  <meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
+  <meta name="keywords" content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta property="og:locale" content="en_US" />
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content="Dobroty od dobrotky" />
+  <link rel="shortcut icon" href="<?php echo url('/');?>/assets/media/logos/favicon.ico" />
+  <!--begin::Fonts-->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+  <!--end::Fonts-->
+  <!--begin::Page Vendor Stylesheets(used by this page)-->
+  <link href="<?php echo url('/');?>/assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
+  <!--end::Page Vendor Stylesheets-->
+  <!--begin::Global Stylesheets Bundle(used by all pages)-->
+  <link href="<?php echo url('/');?>/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+  <link href="<?php echo url('/');?>/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+  <link href="<?php echo url('/');?>/assets/css/style2.css" rel="stylesheet" type="text/css" />
+  <script src='<?php echo url('/') . "/assets/js/jquery.min.js";?>' id='jquery-core-js'></script>
+  <script src='<?php echo url('/') . "/assets/js/jquery-migrate.min.js";?>' id='jquery-migrate-js'></script>
+  <script type="text/javascript" src="https://mapi.trustpay.eu/mapi5/Scripts/TrustPay/popup.js"></script>
+  <!--end::Global Stylesheets Bundle-->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
 
 <div class="d-flex flex-column flex-root">
   <!--begin::Authentication - Sign-in -->
@@ -35,5 +55,3 @@
 </div>
 </div>
 </div>
-
-@include('dopyt/footer')
